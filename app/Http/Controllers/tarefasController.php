183273bbe4026fec8795207fc757998ba3be232a
Alexandre Nascimento <alecatuae@gmail.com>
2020-05-05 20:56:31 -0300

    Configuração do Controller

---
+++ app/Http/Controllers/tarefasController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TarefasController 
+{
+    public function listaTarefas()
+    {
+        $tarefas = [
+            'Estudar Laravel',
+            'Assistir Friends',
+            'Ir ao mercado',
+            'Inscrever no canal do Andrey',
+            'Acompanhar os videos postados',
+            'Tirar 10 na PS'
+        ];
+    
+        $html = '<h1>Tarefas para fazer</h1>';
+        $html .= '<ul>';
+        foreach($tarefas as $tarefa) {
+            $html .= "<li> $tarefa </li>";
+        }
+        $html .= '</ul>';
+        return $html;
+    }
+}

b95ff6434b11a11a9b1cf6d54f9fee7fb9f9b3fa
Alexandre Nascimento <alecatuae@gmail.com>
2020-05-05 22:12:37 -0300

    Inserido Blades

--- app/Http/Controllers/tarefasController.php
+++ app/Http/Controllers/tarefasController.php
@@ -2,25 +2,19 @@
 
 namespace App\Http\Controllers;
 
-class TarefasController 
+class TarefasController
 {
-    public function index()
-    {
-        $tarefas = [
-            'Estudar Laravel',
-            'Assistir Friends',
-            'Ir ao mercado',
-            'Inscrever no canal do Andrey',
-            'Acompanhar os videos postados',
-            'Tirar 10 na PS'
-        ];
-    
-        $html = '<h1>Tarefas para fazer</h1>';
-        $html .= '<ul>';
-        foreach($tarefas as $tarefa) {
-            $html .= "<li> $tarefa </li>";
-        }
-        $html .= '</ul>';
-        return $html;
-    }
+  public function index() 
+  {
+    $tarefas = [
+        'Estudar Laravel',
+        'Assistir Friends',
+        'Ir ao mercado',
+        'Inscrever no canal do Andrey',
+        'Acompanhar os videos postados',
+        'Tirar 10 na PS'
+    ];
+
+    return view('tarefas.index', compact('tarefas'));
+  }
 }
---
+++ resources/views/tarefas/index.blade.php
@@ -0,0 +1,37 @@
+@extends("layout")
+
+@section("conteudo")
+    <h2 class="mt-4">Incluir Nova Tarefa</h2>
+    <form class="form-inline row" method="post">
+      <div class="form-group col-8 mb-2 mr-4">
+        <input type="text" class="form-control col-12" id="tarefa" placeholder="Informe a descrição da sua tarefa">
+      </div>
+      <button type="submit" class="btn btn-dark col-2 mb-2">Adicionar</button>
+    </form>
+    <hr />
+    <h2 class="mt-4">Tarefas para fazer</h2>
+    <table class="table">
+      <thead>
+        <th scope="col">Descrição</th>
+        <th scope="col" style="width: 200px">Ações</th>
+      </thead>
+      <tbody>
+        @foreach($tarefas as $tarefa)
+        <tr>
+          <td>{{ $tarefa }}</td>
+          <td>
+            <a href="#" class="btn btn-success">
+              <i class="fas fa-check"></i>
+            </a>
+            <a href="#" class="btn btn-warning">
+              <i class="fas fa-edit"></i>
+            </a>
+            <a href="#" class="btn btn-danger">
+              <i class="fas fa-trash"></i>
+            </a>
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+    @endsection

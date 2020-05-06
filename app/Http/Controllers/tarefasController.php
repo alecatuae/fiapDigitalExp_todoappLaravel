5f1fd34a94bb9beca323aab9b5480e6da6e55faa
Alexandre Nascimento <alecatuae@gmail.com>
2020-05-05 21:01:20 -0300

    configuracao de boa pratica trocando o metodo listaTarefas por index

--- app/Http/Controllers/tarefasController.php
+++ app/Http/Controllers/tarefasController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 class TarefasController 
 {
-    public function listaTarefas()
+    public function index()
     {
         $tarefas = [
             'Estudar Laravel',
